df6176e31109fb24d4b5a9c36e4900e49366286a
= <=>
2022-02-11 12:58:07 +0700

    commit token list

---
+++ index.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const { writeFileSync, existsSync, mkdirSync } = require("fs");
+const path = require("path");
+
+async function main() {
+  const terraAssetList = await getTerraAsset();
+  const terraswapAssetList = await getTerraswapAsset();
+  const mergedTokens = [];
+  terraswapAssetList.forEach((x) => {
+    const asset = terraAssetList.find((c) => c.token == x.contract_addr);
+    if (asset) {
+      mergedTokens.push({ ...x, ...asset });
+    }
+  });
+
+  const fileName = "token.json";
+  const outdir = path.join(__dirname, "output");
+  const filePath = path.join(outdir, fileName);
+  if (!existsSync(outdir)) {
+    mkdirSync(outdir);
+  }
+
+  writeFileSync(filePath, JSON.stringify(mergedTokens));
+}
+
+async function getTerraAsset() {
+  const terraAssetListUrl = "https://assets.terra.money/cw20/tokens.json";
+  const terraAssetList = await axios.default.get(terraAssetListUrl);
+  const testnetTokens = terraAssetList.data["testnet"];
+  const mainnetToken = terraAssetList.data["mainnet"];
+
+  const testnets = getTokenList(testnetTokens, "testnet");
+  const mainnet = getTokenList(mainnetToken, "mainnet");
+  const terraAssetListFlat = testnets.concat(mainnet);
+  return terraAssetListFlat;
+}
+
+async function getTerraswapAsset() {
+  const terraswapAssetListUrl = "https://api.terraswap.io/tokens";
+  const assetsJson = (await axios.default.get(terraswapAssetListUrl)).data;
+  return assetsJson;
+}
+
+function getTokenList(tokens, network) {
+  const keys = Object.keys(tokens);
+  const tokenList = [];
+  keys.forEach((element) => {
+    tokenList.push({ ...tokens[element], isTestnet: network == "testnet" });
+  });
+  return tokenList;
+}
+
+main();
